93f92097a9707ad5eb7fd012e12ef5a889a85c52
zhangnan <824009273@qq.com>
2019-04-27 15:08:03 +0800

    1

--- AutoTest/test/recover.py
+++ AutoTest/test/recover.py
@@ -1,12 +1,13 @@
 import cPickle as pickle
+import sys
+sys.path.append('..')
 
-from main import conf_file
+from Model.Network import pickle_file
+
 
 if __name__ == '__main__':
     this_dc = 1
-    with open(conf_file, "rb") as f:
-        f1 = f.read()
-        network = pickle.load(f1)
-        f.close()
+    with open(pickle_file, "rb") as f:
+        network = pickle.load(f)
     network.set_up_mininet(this_dc)
 
