761c3e9032590f3fba579bad32dfc7ebaa10fd50
zhangnan <824009273@qq.com>
2019-04-27 14:59:18 +0800

    1

--- AutoTest/test/recover.py
+++ AutoTest/test/recover.py
@@ -5,7 +5,8 @@ from main import conf_file
 if __name__ == '__main__':
     this_dc = 1
     with open(conf_file, "rb") as f:
-        network = pickle.load(f)
+        f1 = f.read()
+        network = pickle.load(f1)
         f.close()
     network.set_up_mininet(this_dc)
 
