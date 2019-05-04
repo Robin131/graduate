5a9adfccc0188942e9fa9683a4e8558f1a13157d
zhangnan <824009273@qq.com>
2019-05-04 21:06:26 +0800

    try exception catch

--- AutoTest/Model/ThreadPool.py
+++ AutoTest/Model/ThreadPool.py
@@ -43,15 +43,24 @@ class Thread(threading.Thread):
     def run(self):
         st = time.time()
         while(True):
-            func, kwargs = self.work_queue.get(block=False)
-            time_seq = kwargs['time_seq']
-            now = time.time()
-            while now - st < time_seq:
-                time.sleep(ThreadParameter.thread_sleep_time)
+            try:
+                func, kwargs = self.work_queue.get(block=False)
+                time_seq = kwargs['time_seq']
                 now = time.time()
-            func(src=kwargs['src'], dst=kwargs['dst'], size=kwargs['size'])
-            print('time:{}, src: {}, dst: {}, size:{}'.format(time_seq, kwargs['src'].ip, kwargs['dst'].ip, kwargs['size']))
-            self.work_queue.task_done()
+                while now - st < time_seq:
+                    time.sleep(ThreadParameter.thread_sleep_time)
+                    now = time.time()
+                func(src=kwargs['src'], dst=kwargs['dst'], size=kwargs['size'])
+                print('time:{}, src: {}, dst: {}, size:{}'.format(time_seq, kwargs['src'].ip, kwargs['dst'].ip, kwargs['size']))
+                self.work_queue.task_done()
+            except Exception as e:
+                # 如果是队列为空错误，则说明所有数据流已经处理完
+                if isinstance(e, Queue.Empty):
+                    break
+                # 若是其他错误，报错但继续
+                else:
+                    print(e.message)
+                    continue
 
 
 
