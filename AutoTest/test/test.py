eb364458aec6eb20032456a7b7be84669f5f1f6b
robin <824009273@qq.com>
2019-05-06 05:34:51 +0000

    1

--- AutoTest/test/test.py
+++ AutoTest/test/test.py
@@ -15,8 +15,6 @@ if __name__ == '__main__':
     h1 = net.addHost('h1', ip='10.0.0.1')
     h2 = net.addHost('h2', ip='10.0.0.2')
     h3 = net.addHost('h3', ip='10.0.0.3')
-    h4 = net.addHost('h4', ip='10.0.0.4')
-    h5 = net.addHost('h5', ip='10.0.0.5')
 
     info('*** Adding switch\n')
     s1 = net.addSwitch('s1')
@@ -24,20 +22,22 @@ if __name__ == '__main__':
     s3 = net.addSwitch('s3')
     s4 = net.addSwitch('s4')
     s5 = net.addSwitch('s5')
+    s6 = net.addSwitch('s6')
 
     info('*** Creating links\n')
     # host 1-4 connect to ports 1-4 on the switch
     net.addLink(h1, s1)
     net.addLink(h2, s2)
     net.addLink(h3, s3)
-    net.addLink(h4, s4)
-    net.addLink(h5, s5)
+    net.addLink(s1, s4)
+    net.addLink(s2, s4)
+    net.addLink(s3, s5)
+    net.addLink(s4, s6)
+    net.addLink(s5, s6)
 
-    net.addLink(s1, s2)
-    net.addLink(s2, s3)
-    net.addLink(s3, s4)
-    net.addLink(s4, s5)
+    # net.addLink(s5, s1)
 
     net.start()
     CLI(net)
     net.stop()
+
