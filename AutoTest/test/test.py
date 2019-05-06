24c48c68a57ac673582f3294b3c01dc00031d739
zhangnan <824009273@qq.com>
2019-05-06 13:20:05 +0800

    test mininet

---
+++ AutoTest/test/test.py
@@ -0,0 +1,38 @@
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.log import info
+
+
+
+if __name__ == '__main__':
+    net = Mininet(controller=Controller)
+
+    info('*** Adding controller\n')
+    net.addController('c0')
+
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1')
+    h2 = net.addHost('h2', ip='10.0.0.2')
+    h3 = net.addHost('h3', ip='10.0.0.3')
+    h4 = net.addHost('h4', ip='10.0.0.4')
+    h5 = net.addHost('h5', ip='10.0.0.5')
+
+    info('*** Adding switch\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+    s4 = net.addSwitch('s4')
+    s5 = net.addSwitch('s5')
+
+    info('*** Creating links\n')
+    # host 1-4 connect to ports 1-4 on the switch
+    net.addLink(h1, s1)
+    net.addLink(h2, s2)
+    net.addLink(h3, s3)
+    net.addLink(h4, s4)
+    net.addLink(h5, s5)
+
+    net.addLink(s1, s2)
+    net.addLink(s2, s3)
+    net.addLink(s3, s4)
+    net.addLink(s4, s5)
