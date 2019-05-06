c71ede258b55fc7f497d3496e9a1fdc95552738c
robin <824009273@qq.com>
2019-05-06 07:31:53 +0000

    1

--- AutoTest/test/test.py
+++ AutoTest/test/test.py
@@ -1,43 +1,5 @@
-from mininet.net import Mininet
-from mininet.node import Controller
-from mininet.log import info
-from mininet.cli import CLI
-
-
+import AutoTest.Model.Util as U
+from AutoTest.Model.const import FilePath
 
 if __name__ == '__main__':
-    net = Mininet(controller=Controller)
-
-    info('*** Adding controller\n')
-    net.addController('c0')
-
-    info('*** Adding hosts\n')
-    h1 = net.addHost('h1', ip='10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.2')
-    h3 = net.addHost('h3', ip='10.0.0.3')
-
-    info('*** Adding switch\n')
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-    s3 = net.addSwitch('s3')
-    s4 = net.addSwitch('s4')
-    s5 = net.addSwitch('s5')
-    s6 = net.addSwitch('s6')
-
-    info('*** Creating links\n')
-    # host 1-4 connect to ports 1-4 on the switch
-    net.addLink(h1, s1)
-    net.addLink(h2, s2)
-    net.addLink(h3, s3)
-    net.addLink(s1, s4)
-    net.addLink(s2, s4)
-    net.addLink(s3, s5)
-    net.addLink(s4, s6)
-    net.addLink(s5, s6)
-
-    # net.addLink(s5, s1)
-
-    net.start()
-    CLI(net)
-    net.stop()
-
+    U.Util.del_file(FilePath.res_path)
